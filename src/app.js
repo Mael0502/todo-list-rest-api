0496421ad90b97eeac5234694db22b76b5f7ac05
Amilcar Diego Revollo Bernal <202300547@est.umss.edu>
2026-05-26 22:49:03 -0400

    Serve React frontend from Express

--- src/app.js
+++ src/app.js
@@ -16,21 +16,27 @@ app.set('views', path.join(__dirname, 'views'));
 app.get('/', (req, res) => {
   res.render('index', {
     title: 'Todo List REST API'
-
   });
 });
 
 app.use('/api/todos', todoRoutes);
 app.use('/todos', todoRoutes);
 
+// Servir React desde /app
 const clientDistPath = path.join(__dirname, '..', 'client', 'dist');
 
 app.use('/app', express.static(clientDistPath));
 
+app.get(/^\/app(\/.*)?$/, (req, res) => {
+  res.sendFile(path.join(clientDistPath, 'index.html'));
+});
+
+// Ruta no encontrada
 app.use((req, res) => {
   res.status(404).json({
     success: false,
     message: 'Ruta no encontrada'
   });
 });
+
 module.exports = app;
